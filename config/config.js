518b68f99335652084db89cfcd034267fd2d6818
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-12-09 11:13:49 +0600

    :wrench: change port number

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3001,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: 'mongodb+srv://mirsahib:mirsahib@mern-skeleton.hzhfa.mongodb.net/project-hathor?retryWrites=true&w=majority'
 }
