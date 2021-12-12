3aed51b7cfbaedcd6d36c158926d079f9f1e0563
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-12-12 16:55:34 +0600

    :hammer: Update model with schema function

--- server/models/customer.model.js
+++ server/models/customer.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
+import crypto from 'crypto'
+
 const CustomerSchema = new mongoose.Schema({
 
   name: {
@@ -16,13 +18,13 @@ const CustomerSchema = new mongoose.Schema({
   orders:[
     {type:Schema.Types.ObjectId,ref:'Order'}
   ],
-  password: {
+  hashed_password: {
     type: String,
     required: "Password is required"
   },
   subscriber:[
     {type:mongoose.Schema.Types.ObjectId}
-  ],
+  ],// seller id subscribe to customer
 
   updated: Date,
   created: {
@@ -30,5 +32,49 @@ const CustomerSchema = new mongoose.Schema({
     default: Date.now
   }
 })
+
+
+CustomerSchema
+  .virtual('password')
+  .set(function (password) {
+    this._password = password
+    this.salt = this.makeSalt()
+    this.hashed_password = this.encryptPassword(password)
+  })
+  .get(function () {
+    return this._password
+  })
+
+  CustomerSchema.path('hashed_password').validate(function (v) {
+  if (this._password && this._password.length < 6) {
+    this.invalidate('password', 'Password must be at least 6 characters.')
+  }
+  if (this.isNew && !this._password) {
+    this.invalidate('password', 'Password is required')
+  }
+}, null)
+
+CustomerSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password
+  },
+  encryptPassword: function (password) {
+    if (!password) return ''
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex')
+    } catch (err) {
+      return ''
+    }
+  },
+  makeSalt: function () {
+    return Math.round((new Date().valueOf() * Math.random())) + ''
+  },
+  getSalt: function(){
+    return this.salt
+  }
+}
 
 export default mongoose.model('Customer', CustomerSchema)
--- server/models/seller.model.js
+++ server/models/seller.model.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+
 const SellerSchema = new mongoose.Schema({
  
   name: {
@@ -13,17 +15,64 @@ const SellerSchema = new mongoose.Schema({
     match: [/.+@.+..+/, 'Please fill a valid email address'],
     required: 'Email is required'
   },
-  password: {
+  hashed_password: {
     type: String,
     required: "Password is required"
   },
 
-  
+  observer:[
+    {type:mongoose.Schema.Types.ObjectId,ref:''}
+  ],// customer id observer by seller
+
+  salt: String,
   updated: Date,
   created: {
     type: Date,
     default: Date.now
   }
 })
+
+SellerSchema
+  .virtual('password')
+  .set(function (password) {
+    this._password = password
+    this.salt = this.makeSalt()
+    this.hashed_password = this.encryptPassword(password)
+  })
+  .get(function () {
+    return this._password
+  })
+
+  SellerSchema.path('hashed_password').validate(function (v) {
+  if (this._password && this._password.length < 6) {
+    this.invalidate('password', 'Password must be at least 6 characters.')
+  }
+  if (this.isNew && !this._password) {
+    this.invalidate('password', 'Password is required')
+  }
+}, null)
+
+SellerSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password
+  },
+  encryptPassword: function (password) {
+    if (!password) return ''
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex')
+    } catch (err) {
+      return ''
+    }
+  },
+  makeSalt: function () {
+    return Math.round((new Date().valueOf() * Math.random())) + ''
+  },
+  getSalt: function(){
+    return this.salt
+  }
+}
 
 export default mongoose.model('Seller', SellerSchema)
