69c3c733019133f90193794f2218564759599226
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-12-12 18:56:29 +0600

    :alembic: working on mongo change stream

--- server/middleware/socket.middleware.js
+++ server/middleware/socket.middleware.js
@@ -1,6 +1,41 @@
 
-// import Product from '../models/product.model'
-// let productChangeStream = Product.watch()
+import Order from '../models/order.model'
+import Customer from '../models/customer.model'
+import Seller from '../models/seller.model'
+const pipeline = []
+const option = {fullDocument: "updateLookup"}
+
+
+Order.watch(pipeline,option).on('change',async function(change){
+    let customerId = change.fullDocument.customerId
+    let status = change.fullDocument.status
+    console.log(change)
+    // query customer table to get subscriber id
+    // try {
+    //     let data = await Customer.findById(customerId).select({_id:0,subscriber:1})
+    //     let subscribers = data.subscriber
+    // // if order is place notify seller
+    // // if order is progress or done notify the customer
+    //     if(status === 'placed'){
+    //         //check if customer has a subscriber
+    //         if(subscribers.length!=0){
+    //             subscribers.forEach(id=>{
+    //                 console.log(id)
+    //             })
+    //         }else{
+    //             console.log('customer has no subcriber')
+    //         }
+    //     }else if(status === 'progress'){
+
+    //     }else{
+
+    //     }
+   
+    // } catch (error) {
+    //     console.log(error)
+    // }
+
+})
 
 
 // const rootSocket = (io)=>{
